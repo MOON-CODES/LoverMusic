4edfd96b99b86ebfa26f7654258c1dc7fe146af9
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-10 14:52:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -34,7 +34,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SPARTENX-OP/VirusMusicBot",
+    "https://github.com/MOON-CODES/LoverMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
